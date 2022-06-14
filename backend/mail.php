75a7d6c99e4bc7c634a8683526efaa6ecd8d48e7
nesta.web <Nesta.loisy@gmail.com>
2022-06-14 10:51:02 +0200

    send email for contact

--- backend/mail.php
+++ backend/mail.php
@@ -1,4 +1,17 @@
 <?php
 
-mail("nesta.loisy@gmail.com", "Essai", "Coucou les amis", "");
-echo "done";
+$to = "nesta.loisy@gmail.com";
+
+$subject = "Nouveau message de" . $_POST["firstName"] . " " . $_POST["lastName"];
+
+$message = "Numéro de téléphone : " . $_POST["phone"] . "\nE-mail : " . $_POST["email"] . "\n" . $_POST["message"];
+$message = wordwrap($message, 70, "\n");
+
+$headers = [
+    "From" => "contcat@villaborddemercorse.com",
+    "Reply-To" => $_POST["email"]
+];
+
+mail($to, $subject, $message, "From:contcat@villaborddemercorse.com\nReply-To:" . $_POST["email"]);
+
+header("Location: ../index.php");
