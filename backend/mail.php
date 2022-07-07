7bef6996818d22a5a2c4be3698c8aabca0459dfd
nesta.web <Nesta.loisy@gmail.com>
2022-07-07 11:06:52 +0200

    reset db + error mail name

--- backend/mail.php
+++ backend/mail.php
@@ -7,10 +7,10 @@ $subject = "Nouveau message de " . $_POST["firstName"] . " " . $_POST["lastName"
 $message = "Ce message vous a été envoyé via le formulaire de contact du site villaborddemercorse.com\n\nNuméro de téléphone : " . $_POST["phone"] . "\nE-mail : " . $_POST["email"] . "\n\n" . $_POST["message"];
 
 $headers = [
-    "From" => "contcat@villaborddemercorse.com",
+    "From" => "contact@villaborddemercorse.com",
     "Reply-To" => $_POST["email"]
 ];
 
-mail($to, $subject, $message, "From:contcat@villaborddemercorse.com\nReply-To:" . $_POST["email"]);
+mail($to, $subject, $message, "From:contact@villaborddemercorse.com\nReply-To:" . $_POST["email"]);
 
 header("Location: ../index.php");
